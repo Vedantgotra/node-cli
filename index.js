7577de8345f973015ff4e703f15236936fc8c6ca
Vedant Gotra <88156427+Vedantgotra@users.noreply.github.com>
2024-08-28 14:58:35 +0000

    some changes

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-const commander = require("commander");
+const { program } = require("commander");
+
+program.option("--first").option("-s, --separator <char>");
+
+program.parse();
+
+const options = program.opts();
+const limit = options.first ? 1 : undefined;
+console.log(program.args[0].split(options.separator, limit));
